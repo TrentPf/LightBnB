758d25a96ce80c10b794eaf3993f6e17721be1dd
Trent Pfeiffer <trentpf@hotmail.com>
2021-11-24 11:14:34 -0500

    Add data, also minor fix for table

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE users (
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
-  owned_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
   title VARCHAR(255) NOT NULL,
   description TEXT,
